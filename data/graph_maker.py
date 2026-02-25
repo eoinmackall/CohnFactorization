bf5e44fe77aa06745e7732c7194f0972aafe0fc5
Eoin Mackall <eoinmackall@gmail.com>
2026-02-24 23:29:44 -0800

    Testing, testing, testing

--- data/graph_maker.py
+++ data/graph_maker.py
@@ -14,6 +14,11 @@ is_semiprime = (data['divisors'] == 4)
 semiprimes = data['num'][is_semiprime]
 poly_div_for_semis = data['poly_divisors'][is_semiprime]
 
+###### Random samples #######
+sample_size = 10000
+random_indices_full = np.random.choice(len(data['num']), size = sample_size, replace = False)
+random_indices_semiprimes = np.random.choice(len(semiprimes), size = sample_size, replace = False)
+
 #####################################
 #
 # graphs below
@@ -26,15 +31,16 @@ num_semiprimes = len(semiprimes)
 ####### Scatter plots #########
 
 # Integers vs divisors
-plt.scatter(data['num'], data['divisors'])
+plt.scatter(data['num'][random_indices_full], data['divisors'][random_indices_full])
 plt.show()
 
 # Integers vs poly_divisors
-plt.scatter(data['num'], data['poly_divisors'])
+plt.scatter(data['num'][random_indices_full], data['poly_divisors'][random_indices_full])
 plt.show()
 
 # Semi-primes vs poly_divisors
-plt.scatter(semiprimes, poly_div_for_semis)
+plt.scatter(semiprimes[random_indices_semiprimes], poly_div_for_semis[random_indices_semiprimes])
+plt.title(f"Semiprimes {len(semiprimes)}")
 plt.show()
 
 # ?(p-q)/n vs poly_divisors
